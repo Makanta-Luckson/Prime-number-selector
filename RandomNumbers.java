22a6e4e83712d70a7eec822c5296ffbf1f7e90b4
Lucky <makantaluckson71@gmail.com>
2020-10-26 12:28:31 +0200

    Add  two loops the first one generate and stores random numbers into an array and the last one prints them all out

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -3,11 +3,25 @@ import java.util.*;
 public class RandomNumbers{
     public static void main(String[] args) {
         
+   Scanner input = new Scanner(System.in);
+   System.out.println("Enter the limit of the number you want");
+   int num = input.nextInt();
+
+   Random ran = new Random();
+  
+    int array[] = new int[num];
+
+        for(int i = 0; i <= array.length-1; i++){
+                   int limit =  ran.nextInt(10) + 1;
+        }
+
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
 
 
 
 
-        
 
     }
 }
